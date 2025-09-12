3e822e3b5e71b572bf91582c1718ea736c1cf4ad
6emiliano <evvazque@quilmes.com.ar>
2025-09-12 13:24:41 -0300

    Subida inicial

---
+++ main.js
@@ -0,0 +1,48 @@
+//VARIABLE: ESPACIO DE MEMOERIA PARA ALMACENAR INFO promp confirm y alert. console.log()  alert()
+
+
+//Declaro
+
+let cliente
+//Asigno
+
+cliente = "Emiliano"
+cliente = "Samuel"
+
+console.log(cliente)
+//Inicio:
+
+let edad = 37
+edad = 39
+
+console.log(edad)
+
+const nacimiento = 1987
+console.log(nacimiento)
+let suma = 10 + 21
+
+//solo mira para arriba
+if (suma > 20) {
+  console.log("La suma es mayor que 20");
+}
+else {
+  console.log("La suma es menor o igual a 20");
+}
+suma = 10
+
+
+
+console.log(suma)
+
+
+
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
